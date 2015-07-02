9cedbe8dc519b3dd661fc74ab3ab92819f5e7168
kobayashi <kobayashi@level-five.jp>
2015-07-02 18:20:23 +0900

    first commit :wq

---
+++ sql/ken.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS premember;
+CREATE TABLE m_ken (
+	id		SMALLINT,
+	ken     VARCHAR(10),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO m_ken (id, ken) VALUES( 1,'北海道');
+INSERT INTO m_ken (id, ken) VALUES( 1,'青森県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'岩手県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'宮城県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'秋田県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'山形県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'福島県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'茨城県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'栃木県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'群馬県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'埼玉県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'千葉県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'東京都');
+INSERT INTO m_ken (id, ken) VALUES( 1,'神奈川県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'新潟県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'富山県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'石川県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'福井県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'山梨県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'長野県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'岐阜県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'静岡県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'愛知県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'三重県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'滋賀県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'京都府');
+INSERT INTO m_ken (id, ken) VALUES( 1,'大阪府');
+INSERT INTO m_ken (id, ken) VALUES( 1,'兵庫県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'奈良県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'和歌山県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'鳥取県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'島根県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'岡山県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'広島県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'山口県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'徳島県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'香川県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'愛媛県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'高知県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'福岡県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'佐賀県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'長崎県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'熊本県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'大分県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'宮崎県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'鹿児島県');
+INSERT INTO m_ken (id, ken) VALUES( 1,'沖縄県');
